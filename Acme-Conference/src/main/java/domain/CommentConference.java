f4cc717d1a3989bfc39d443fcac9213425ddab0f
Acme <AcmeHR222@gmail.com>
2019-07-21 20:03:43 +0200

    Comentarios cambiados

---
+++ Acme-Conference/src/main/java/domain/CommentConference.java
@@ -0,0 +1,27 @@
+
+package domain;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+
+@Entity
+@Access(AccessType.PROPERTY)
+public class CommentConference extends Commentary {
+
+	private Conference	conference;
+
+
+	@Valid
+	@ManyToOne(optional = true)
+	public Conference getConference() {
+		return this.conference;
+	}
+
+	public void setConference(final Conference conference) {
+		this.conference = conference;
+	}
+
+}
---
+++ Acme-Conference/src/main/java/domain/Commentary.java
@@ -0,0 +1,69 @@
+
+package domain;
+
+import java.util.Date;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.SafeHtml;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Access(AccessType.PROPERTY)
+public abstract class Commentary extends DomainEntity {
+
+	private String	title;
+	private Date	moment;
+	private String	author;
+	private String	text;
+
+
+	@NotBlank
+	@SafeHtml
+	public String getTitle() {
+		return this.title;
+	}
+
+	public void setTitle(final String title) {
+		this.title = title;
+	}
+
+	@NotNull
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@Past
+	@Temporal(TemporalType.TIMESTAMP)
+	public Date getMoment() {
+		return this.moment;
+	}
+
+	public void setMoment(final Date moment) {
+		this.moment = moment;
+	}
+
+	@NotBlank
+	@SafeHtml
+	public String getAuthor() {
+		return this.author;
+	}
+
+	public void setAuthor(final String author) {
+		this.author = author;
+	}
+
+	@NotBlank
+	@SafeHtml
+	public String getText() {
+		return this.text;
+	}
+
+	public void setText(final String text) {
+		this.text = text;
+	}
+}
